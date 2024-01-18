2bc71004dc6efa7928c57fbf291c4a0a56d69af5
dmitrij-pularija <dmitrij.pularija@gmail.com>
2024-01-18 15:50:10 +0200

    fix status

--- decorators/ctrlWrapper.js
+++ decorators/ctrlWrapper.js
@@ -2,7 +2,8 @@ const ctrlWrapper = (ctrl) => {
   const func = async (req, res, next) => {
     try {
       const result = await ctrl(req, res, next);
-      return res.status(200).json(result);
+      res.sendStatus(200);
+      // return res.status(200);
     } catch (error) {
     //   console.log(error);  
       next(error);
