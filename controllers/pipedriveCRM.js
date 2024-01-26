991f8ba9f7bb52ca3d011f28094f7acbb7ed688b
dmitrij-pularija <dmitrij.pularija@gmail.com>
2024-01-26 04:58:38 +0200

    fix notes

--- controllers/pipedriveCRM.js
+++ controllers/pipedriveCRM.js
@@ -37,17 +37,20 @@ const {
       } = req;
       let personsId = '';
       let label = '24';
+      let labelColor = '#a0419d';
       let source = '(main)';
       let foundedPerson = [];
       const referer = req.get("Referer");
       if (referer && referer.includes("/ex1")) {
         label = '23';
+        labelColor = '#d63033';
         if (formid && formid === 'form610726258') source = 'Ex Ф1';
         if (formid && formid === 'form625077433') source = 'Ex pop-up';
         if (formid && formid !== 'form625077433' && formid !== 'form610726258') source = 'Ex Ф2';
       }
       if (referer && referer.includes("/lm1")) {
         label = '22';
+        labelColor = '#4164d4';
         if (formid && formid === 'form572806238') source = 'Lm Ф1';
         if (formid && formid !== 'form572806238') source = 'Lm Ф2';
       }
@@ -63,9 +66,14 @@ const {
       const personPhone = Phone ? Phone : phone2 ? phone2 : "";
       const personEmail = Email ? Email : email2 ? email2 : "";
       const title = `Клієнт залишив заявку на${!source.includes('Lm') ? " консультацію" : ''} ${source}`;
-      const data = role ? ` Результат опитування: Роль? ${role}, Кількість продавців? ${quantity}${volume ? `, Обʼєм продажів? ${volume}, Функція керівника? ${func}, Які проблеми? ${probl}` : ""}` : '';
-      const content = `Клієнт ${personName} ${personPhone} ${personEmail} залишив повторну заявку на${!source.includes('Lm') ? " консультацію, джерело " : ''} ${source}${data}`;
-      const content1 = `Заявка від клієнта ${personName} ${personPhone} ${personEmail} на${!source.includes('Lm') ? " консультацію, джерело " : ''} ${source}${data}`;
+      const data = role ? `<table><caption><h3>Результат опитування&nbsp;</h3></caption><tr><td>Роль:</td><td>${role}</td></tr><tr><td>Кількість продавців:</td><td>${quantity}</td></tr>${volume ? `<tr><td>Обʼєм продажів:</td><td>${volume}</td></tr><tr><td>Функція керівника:</td><td>${func}</td></tr><tr><td>Які проблеми:</td><td><span>${probl.replace(";", "</span></br><span>")}</span></td></tr>` : ""}</table>` : "";
+      const content = `<h2>Клієнт залишив повторну заявку на <span style='background-color: #ffffff; color: ${labelColor};'>${source}</span>&nbsp;&nbsp;&nbsp;</h2><div><table><caption><h3>Контактна інформація&nbsp;</h3></caption><tr><td>Імʼя:</td><td>${personName}&nbsp;</td></tr><tr><td>Телефон:</td><td>${personPhone}&nbsp;</td></tr>${personEmail ? `<tr><td>Email:</td><td>${personEmail}&nbsp;&nbsp;</td></tr>` : "&nbsp;&nbsp;"}</table></br>${data}`;
+      const content1 = `<h2>Клієнт залишив заявку на <span style='background-color: #ffffff; color: ${labelColor};'>${source}</span>&nbsp;&nbsp;&nbsp;</h2><div><table><caption><h3>Контактна інформація&nbsp;</h3></caption><tr><td>Імʼя:</td><td>${personName}&nbsp;</td></tr><tr><td>Телефон:</td><td>${personPhone}&nbsp;</td></tr>${personEmail ? `<tr><td>Email:</td><td>${personEmail}&nbsp;&nbsp;</td></tr>` : "&nbsp;&nbsp;"}</table></br>${data}`;
+      
+      // const data = role ? ` Результат опитування: Роль? ${role}, Кількість продавців? ${quantity}${volume ? `, Обʼєм продажів? ${volume}, Функція керівника? ${func}, Які проблеми? ${probl}` : ""}` : '';
+      // const content = `Клієнт ${personName} ${personPhone} ${personEmail} залишив повторну заявку на${!source.includes('Lm') ? " консультацію, джерело " : ''} ${source}${data}`;
+      // const content1 = `Заявка від клієнта ${personName} ${personPhone} ${personEmail} на${!source.includes('Lm') ? " консультацію, джерело " : ''} ${source}${data}`;
+      // <h2>Клієнт залишив заявку на на <span style='background-color: #ffffff; color: #4164d4;'>Lm Ф1</span>&nbsp;&nbsp;&nbsp;</h2><div><table><caption><h3>Контактна інформація&nbsp;</h3></caption><tr><td>Імʼя:</td><td>Тest Test&nbsp;</td></tr><tr><td>Телефон:</td><td>+380 (50) 000-00-10&nbsp;</td></tr><tr><td>Email:</td><td>test03@gmail.com&nbsp;&nbsp;</td></tr></table></br><table><caption><h3>Результат опитування&nbsp;</h3></caption><tr><td>Роль:</td><td>Інша роль</td></tr><tr><td>Кількість продавців:</td><td>4</td></tr><tr><td>Обʼєм продажів:</td><td>вище 50.000$</td></tr><tr><td>Функція керівника:</td><td>Ніхто</td></tr><tr><td>Які проблеми:</td><td><span>Низька мотивація у відділі продажів;</span></br><span>Відсутність системи та стабільності;</span></br><span>Відсутність інструментів: скрипитів, CRM, регламентів, періоду адаптації і т.д.</span></td></tr></table></div>
       // console.log(referer);
       // console.log("source:", source);
       // console.log(req.body);
